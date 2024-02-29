b846bcc76daf731dc7ac5bf622eaa3133d9e1232
norma <norma@normas-MacBook-Pro.local>
2024-02-29 18:27:20 +0900

    first commit

---
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -0,0 +1,26 @@
+package hello.itemservice.domain.item;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
+
+@Data
+public class Item {
+
+    private Long id;
+    private String itemName;
+    private Integer price;
+    private Integer quantity;
+
+    public Item() {
+    }
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
